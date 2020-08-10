e1d9ea2a6b752e29ce8caa17cf5c038e80880334
rafaelsantos858 <69479684+rafaelsantos858@users.noreply.github.com>
2020-08-10 16:47:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// function addponto(){
+//     var cpf = document.getElementById("cpf")
+//     if(cpf.value.length == 3){
+//         cpf.value += "."
+//     }
+//     if(serie.value.length == 7){
+//         cpf.value += "."
+//     }
+//     if(serie.value.length == 11){
+//         cpf.value += "-"
+//     }
+// }
+
+// function addponto2(){
+//     var rg = document.getElementById("rg")
+//     if(serie.value.length == 2){
+//         rg.value += "."
+//     }
+//     if(rg.value.length == 6){
+//         rg.value += "."
+//     }
+//     if(rg.value.length == 10){
+//         rg.value += "-"
+//     }
+// }   
